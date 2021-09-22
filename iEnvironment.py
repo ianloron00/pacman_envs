546745490c79c00a1f1e5ad22f996ec0c0837f97
Mr. Ian <ianloron00@gmail.com>
2021-09-22 14:06:59 -0300

    🔀: net_arch changed from 64x64 to 128x64

---
+++ iEnvironment.py
@@ -0,0 +1,197 @@
+# Written by Ian Loron de Almeida.
+
+from os import stat_result
+from gym.core import RewardWrapper
+from numpy.lib.utils import info
+
+import math, util
+from tokenize import String
+import gym
+from gym import spaces
+from gym.spaces.discrete import Discrete
+from gym.utils import seeding
+import numpy as np
+
+from pacman import *
+from game import *
+from iUpdate import PacmanState as Update
+from iFeatures import *
+ 
+# Create an environment that is equivalent to Gym's standard environments.
+# It should contain:
+"""
+.render()
+.step(action)
+.reset()
+.close()
+.seed(seed=None)
+"""
+
+class PacmanEnv(gym.Env):
+    metadata = {
+        "render.modes": ["human", "rgb_array"],
+    }
+
+    POSSIBLE_ACTIONS = np.array([Directions.STOP, Directions.NORTH, Directions.SOUTH, 
+                                Directions.EAST, Directions.WEST])
+
+    def __init__(self, maze_name='mediumClassic',extractor='BoardState'):
+        
+        super(PacmanEnv, self).__init__()
+        
+        self.args = ""
+        self.game = None
+        self.rules = None
+
+        self.action_space = None
+        self.observation_space = None
+        self.converted_action_space = self.POSSIBLE_ACTIONS
+
+        self.reward = 0
+        self.last_score = 0
+        self.last_action = None
+        self.info = {}
+
+        self.featExtractor = util.lookup(extractor, globals())()
+        self.features = np.array([])
+        self.frame = np.array([])
+        self.state = np.array([])
+
+        self.timestep = 0
+        self.eps = 0
+
+        self.n_food = 0
+
+
+    def make(extractor='BoardState', **args):
+        # create pacman environment
+        print(extractor)
+        pacman = PacmanEnv(extractor = extractor)
+
+        # initialize a game
+        pacman.initialize(**args)
+        return pacman
+    
+
+    def reset(self):
+        self.eps += 1
+        self.timestep = 0
+
+        if not (self.eps % 2000): print("eps: {}".format(self.eps))
+        
+        # reset reward variables
+        self.reward = 0
+        self.last_score = 0
+
+        # reset last action
+        self.last_action = None
+        
+        # create game        
+        self.game = self.createGame(**self.args.copy())
+
+        # reset state-related variables
+        Update.reset(self)
+
+        # should return state.
+        return self.state
+
+
+    def render(self, mode='human'):
+        self.game.render = True
+
+
+    def step(self, action):
+        if not (self.eps % 2000):
+            print(str((action, self.valueToAction(action), self.reward)))
+        
+        # convert action to Discrete.
+        action = self.valueToAction(action)
+
+        self.game.step(action=action)
+
+        # update features
+        self.frame = Update.get_frame(self, self.game.state, action)
+
+        # update state
+        self.state = Update.get_state(self)
+        
+        # update reward
+        Update.update_reward(self, self.game.state.data.score, action)
+
+        # update last action
+        self.last_action = action
+
+        # update timestep
+        self.timestep += 1
+
+        return (self.state, self.reward, self.game.gameOver, self.info)
+    
+
+    def close(self):
+        self.game.display.finish() 
+
+
+    def validade_action(self, action):
+        if action not in self.game.state.getLegalPacmanActions():
+            return Directions.STOP
+        return action
+
+
+    def ActionToValue(self, action):
+        if type(action) == type(Directions.STOP):
+            return np.where(action == self.converted_action_space)
+
+
+    def valueToAction(self, action):
+        # actions can be either numbers or Directions.
+        if type(action) != type(Directions.STOP):
+            action = self.converted_action_space[action]
+
+        return self.validade_action(action)
+
+
+    """
+    'initialize' takes all parameters and convert to a string, to reuse former pacman functions. 
+    """
+    def initialize(self, extractor='BoardState', layout=None, numTraining=None, numGames=None, numGhosts=None, zoom=None, display=2):
+        
+        myArgs=[]
+        
+        if layout: myArgs.append("-l"+layout)
+        if numTraining: myArgs.append("-x "+str(numTraining))
+        if numGames: myArgs.append("-n "+str(numGames))
+        if numGhosts: myArgs.append("-k "+str(numGhosts))
+        if zoom: myArgs.append("-z "+str(zoom))
+        if display == 0: myArgs.append('-q')
+        elif display == 1: myArgs.append('-t')
+
+        self.args = readCommand(myArgs)
+        self.game = self.createGame(**self.args.copy())
+
+        self.initialize_action_space()
+        self.initialize_observation_space()
+
+        food = np.array(self.game.state.getFood().data)
+        self.n_food = len(food[food == True])
+
+    def initialize_action_space(self):      
+        # set action space
+        self.action_space = Discrete(5)
+
+
+    def initialize_observation_space(self):
+        Update.initialize_observation_space(self)
+
+
+    def createGame(self, layout, pacman, ghosts, display, numGames, record=False, numTraining = 0, catchExceptions=False, timeout=30 ):
+        import __main__
+        __main__.__dict__['_display'] = display
+
+        rules = ClassicGameRules(timeout)
+        return rules.newGame(layout, pacman, ghosts, display)
+
+
+    # returns random Direction.
+    def getRandomAction(self):
+        legal = self.game.state.getLegalPacmanActions()
+        return np.random.choice(legal) if legal != [] else None
---
+++ iMain.py
@@ -0,0 +1,108 @@
+"""
+2- Treinar mais o pacman
+
+--------------------------
+- estado do pacman alterado. 
+- Alterado gráfico de curva de aprendizado, para apresentar desvio padrao
+"""
+
+"""
+https://stable-baselines3.readthedocs.io/en/master/modules/dqn.html
+
+CnnPolicy - para Imagens
+MultiInputPolicy - para estados do tipo dicionário
+
+"learning_rate deveria ser alterado? (padrao 0.0001)"
+"""
+
+import numpy as np
+
+import gym, os
+from stable_baselines3 import DQN
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.evaluation import evaluate_policy
+# from stable_baselines3.common.results_plotter import plot_results
+# test
+from stable_baselines3.common.utils import get_linear_fn
+
+from iEnvironment import *
+from iCallback import *
+from iGraphs import *
+
+# 'BoardState' | 'Selected'
+extractor = 'BoardState'
+name_model = 'discrete_pacman_' + extractor
+directory = "tmp/"
+isTraining = False
+
+start = 0.30
+end = 0.05
+fraction = 1.0
+
+env = PacmanEnv.make(extractor=extractor, zoom=2.0)
+env = Monitor(env, directory) 
+
+#### applying stable_baselines models.
+model = DQN('MlpPolicy', env, tensorboard_log=directory, buffer_size=500000, 
+            learning_starts=10000, exploration_fraction=1.0, exploration_initial_eps=1.0, 
+            exploration_final_eps=0.65, verbose=0, policy_kwargs=dict( net_arch=[128, 64] ))
+
+def set_expl_variables(model, start=1.0, end=0.05, fraction=1.0):
+    model.exploration_fraction = fraction
+    model.exploration_initial_eps = start
+    model.exploration_final_eps = end
+    model.exploration_rate = model.exploration_initial_eps
+    model._setup_model()
+
+
+if not os.path.exists(directory):
+    os.mkdir(directory)
+
+
+if os.path.exists(directory + name_model+".zip"):
+    model = model.load(directory + name_model + ".zip", env=env)    
+    print("model loaded.")
+
+    set_expl_variables(model, start, end, fraction)
+
+
+elif os.path.exists(directory + name_model + "/" + name_model+".zip"):
+    model = model.load(directory + name_model + "/" + name_model+".zip", env=env)
+    print("callback loaded.")
+
+    set_expl_variables(model, start, end, fraction)
+
+
+if isTraining:
+    timesteps = 1e6
+
+    print("initial: {}, final: {}, frac: {}, progr: {}".format(
+                        model.exploration_initial_eps, 
+                        model.exploration_final_eps, 
+                        model.exploration_fraction,
+                        model._current_progress_remaining))
+
+    callback = SaveOnBestTrainingRewardCallback(check_freq=50000, log_dir=directory, name=name_model)
+    model.learn(total_timesteps=timesteps, callback=callback)
+    
+    # plot rewards of training
+    plt_training([directory], timesteps, results_plotter.X_TIMESTEPS, "DQN Pacman", name_model)
+
+    # double-save
+    model.save(directory)
+    print("model saved.")
+
+else:
+    rewards = evaluate_policy(model, env, n_eval_episodes=100, render=False, return_episode_rewards=True)
+
+    # plot results from policy evaluation
+    scores=rewards[0]
+
+    plt.figure(figsize=(20,10))
+    plt.plot(np.arange(len(scores)) + 1, scores)
+    plt.title("Pacman reward evaluation | DQN")
+    plt.savefig(directory + "Pacman R eval. | extractor " + extractor + " | DQN" + '.png')
+    plt.show()
+    plt.close()
+
+env.close()
